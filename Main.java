e8673f386cfa3a8cc23e47c1fd560bb1737b7de7
G1t-hub-user1789 <turarb2006@gmail.com>
2025-04-13 15:54:49 +0500

    Main.java

--- Main.java
+++ Main.java
@@ -1,11 +1,35 @@
 public class Main {
     public static void main(String[] args) {
+        // Test MyArrayList
+        MyArrayList<Integer> arrayList = new MyArrayList<>();
+        arrayList.add(10);
+        arrayList.add(20);
+        arrayList.add(30);
+        System.out.println("MyArrayList: " + arrayList.get(1)); // 20
+
+        // Test MyLinkedList
+        MyLinkedList<String> linkedList = new MyLinkedList<>();
+        linkedList.add("Hello");
+        linkedList.add("World");
+        System.out.println("MyLinkedList: " + linkedList.get(0)); // Hello
+
+        // Test MyStack
+        MyStack<Integer> stack = new MyStack<>();
+        stack.push(1);
+        stack.push(2);
+        System.out.println("MyStack pop: " + stack.pop()); // 2
+
+        // Test MyQueue
+        MyQueue<String> queue = new MyQueue<>();
+        queue.enqueue("A");
+        queue.enqueue("B");
+        System.out.println("MyQueue dequeue: " + queue.dequeue()); // A
+
+        // Test MyMinHeap
         MyMinHeap<Integer> heap = new MyMinHeap<>();
-        heap.insert(4);
-        heap.insert(2);
-        heap.insert(7);
-        heap.insert(1);
-        System.out.println(heap.extractMin()); // 1
-        System.out.println(heap.getMin());     // 2
+        heap.insert(50);
+        heap.insert(10);
+        heap.insert(30);
+        System.out.println("MyMinHeap min: " + heap.getMin()); // 10
     }
 }
